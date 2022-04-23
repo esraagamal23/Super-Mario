623b15c7c81976ee386f72e15d32b95758123a25
Rawaa Ahmed <60402572+rawaa-ahmed@users.noreply.github.com>
2022-04-23 20:58:32 +0200

    Add files via upload

--- CMPN205 Phase 2+3 - Student Version/source/common/texture/texture-utils.cpp
+++ CMPN205 Phase 2+3 - Student Version/source/common/texture/texture-utils.cpp
@@ -2,9 +2,7 @@
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb/stb_image.h>
-
 #include <iostream>
-
 our::Texture2D* our::texture_utils::empty(GLenum format, glm::ivec2 size){
     our::Texture2D* texture = new our::Texture2D();
     //TODO: (Req 10) Finish this function to create an empty texture with the given size and format
@@ -35,7 +33,14 @@ our::Texture2D* our::texture_utils::loadImage(const std::string& filename, bool 
     our::Texture2D* texture = new our::Texture2D();
     //Bind the texture such that we upload the image data to its storage
     //TODO: (Req 4) Finish this function to fill the texture with the data found in "pixels" and generate the mipmaps if requested
-
+    texture->bind();
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, size.x, size.y,0, GL_RGBA,
+    GL_UNSIGNED_BYTE, pixels);
+    if(generate_mipmap)
+    {
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
     stbi_image_free(pixels); //Free image data after uploading to GPU
     return texture;
 }
