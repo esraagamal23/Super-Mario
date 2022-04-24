da8e9c81082b8fb8c4770fde25bd81b3250c4bda
esraa <gamalesraa868@gmail.com>
2022-04-24 20:13:24 +0200

    Req 2(Transform)

--- CMPN205 Phase 2+3 - Student Version/source/common/ecs/transform.cpp
+++ CMPN205 Phase 2+3 - Student Version/source/common/ecs/transform.cpp
@@ -10,7 +10,20 @@ namespace our {
     // HINT: to convert euler angles to a rotation matrix, you can use glm::yawPitchRoll
     glm::mat4 Transform::toMat4() const {
         //TODO: (Req 2) Write this function
-        return glm::mat4(1.0f);
+     
+     //translation matrix
+        glm::mat4 translate = glm::mat4(1.0f);
+        translate =glm::translate(translate, position);
+        //scaling matrix
+        glm::mat4  Scale = glm::mat4(1.0f);
+        Scale = glm::scale( Scale,scale); 
+        //rotation matrix
+         glm::mat4  Rotate = glm::mat4(1.0f);
+       Rotate =glm::yawPitchRoll(rotation.y,rotation.x,rotation.z);
+         glm::mat4 transform =translate*Rotate* Scale; 
+
+        return transform;
+        
     }
 
      // Deserializes the entity data and components from a json object
