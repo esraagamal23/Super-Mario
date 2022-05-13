c344c6ea4574e789fa569f10557d0501933b92d2
Omar A.Rezk <62177195+Omar-ahmed314@users.noreply.github.com>
2022-05-13 14:59:07 +0200

    complete the projection matrix

--- CMPN205 Phase 2+3 - Student Version/source/common/components/camera.cpp
+++ CMPN205 Phase 2+3 - Student Version/source/common/components/camera.cpp
@@ -35,7 +35,13 @@ namespace our {
         // - the center position which is the point (0,0,-1) but after being transformed by M
         // - the up direction which is the vector (0,1,0) but after being transformed by M
         // then you can use glm::lookAt
-        return glm::mat4(1.0f);
+        glm::mat4 cameraViewMatrix = glm::lookAt(
+            glm::vec3(0, 0, 0),
+            glm::vec3(0, 0, -1),
+            glm::vec3(0, 1, 0)
+        );
+        
+        return cameraViewMatrix * M;
     }
 
     // Creates and returns the camera projection matrix
@@ -46,6 +52,25 @@ namespace our {
         // It takes left, right, bottom, top. Bottom is -orthoHeight/2 and Top is orthoHeight/2.
         // Left and Right are the same but after being multiplied by the aspect ratio
         // For the perspective camera, you can use glm::perspective
-        return glm::mat4(1.0f);
+        glm::mat4 projectionMatrix;
+        if(cameraType == CameraType::ORTHOGRAPHIC)
+        {
+            projectionMatrix = glm::ortho(
+                float(viewportSize[0]), 
+                float(viewportSize[1]), 
+                -orthoHeight / 2.0f, 
+                orthoHeight / 2.0f
+            );
+        } 
+        else
+        {
+            projectionMatrix = glm::perspective(
+                fovY,
+                viewportSize[0] / float(viewportSize[1]),
+                near,
+                far
+            );
+        }
+        return projectionMatrix;
     }
 }
